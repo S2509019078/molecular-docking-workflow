c697230e911237021a41de84eb3aa49db4a62d2e
Zhang Shunde <S2509019078@neau.edu.cn>
2026-07-12 01:14:59 +0800

    ui: launch scientific workflow desktop

--- src/dockflow/gui_launcher.py
+++ src/dockflow/gui_launcher.py
@@ -21,8 +21,7 @@ def main(argv=None) -> int:
     args = list(sys.argv[1:] if argv is None else argv)
     smoke_test = args == ["--gui-smoke-test"]
     try:
-        from dockflow.gui_preview import run_gui
-
+        from dockflow.gui_scientific import run_gui
         return int(run_gui(_default_runs_dir(), smoke_test=smoke_test) or 0)
     except Exception:
         details = traceback.format_exc()
